708f6cf5ddf86e474c1f3c12cdd746e5420e1e6b
Oleg Habchak <71442227+oleghabchak@users.noreply.github.com>
2023-09-12 21:11:19 +0300

    fix lint

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -9,19 +9,12 @@ const deviceManagerEmitteSIO = new NativeEventEmitter(sioModule);
 
 export default function App() {
   useEffect(() => {
-    const subscription = deviceManagerEmitterSDM.addListener('sdmEventNewDexData', event => {
-      console.log('sdmEventNewDexData' + JSON.stringify(event));
-    });
-
-    return () => {
-      subscription.remove();
-    }
-  }, []);
-
-  useEffect(() => {
-    const subscription = deviceManagerEmitteSIO.addListener('sioDexEventNewData', event => {
-      console.log('sioDexEventNewData' + JSON.stringify(event));
-    });
+    const subscription = deviceManagerEmitterSDM.addListener(
+      'sdmEventNewDexData',
+      (event) => {
+        console.log('sdmEventNewDexData' + JSON.stringify(event));
+      }
+    );
 
     return () => {
       subscription.remove();
@@ -29,9 +22,25 @@ export default function App() {
   }, []);
 
   useEffect(() => {
-    const subscription = deviceManagerEmitteSIO.addListener('sioDexEventTrace', event => {
-      console.log('sioDexEventTrace' + JSON.stringify(event));
-    });
+    const subscription = deviceManagerEmitteSIO.addListener(
+      'sioDexEventNewData',
+      (event) => {
+        console.log('sioDexEventNewData' + JSON.stringify(event));
+      }
+    );
+
+    return () => {
+      subscription.remove();
+    };
+  }, []);
+
+  useEffect(() => {
+    const subscription = deviceManagerEmitteSIO.addListener(
+      'sioDexEventTrace',
+      (event) => {
+        console.log('sioDexEventTrace' + JSON.stringify(event));
+      }
+    );
 
     return () => {
       subscription.remove();
@@ -40,23 +49,26 @@ export default function App() {
 
   useEffect(() => {
     console.log('add listener for sdmEventDeviceState');
-    const subscription = deviceManagerEmitterSDM.addListener('sdmEventDeviceState', event => {
-      console.log('New device state ' + event.address + ': ' + event.state);
-    });
+    const subscription = deviceManagerEmitterSDM.addListener(
+      'sdmEventDeviceState',
+      (event) => {
+        console.log('New device state ' + event.address + ': ' + event.state);
+      }
+    );
 
     return () => {
       subscription.remove();
-    }
+    };
   });
 
   return (
     <View style={styles.container}>
       <Button
-        title='Call showDeviceManager'
+        title="Call showDeviceManager"
         onPress={() => showDeviceManager()}
       />
       <Button
-        title='Dex Download'
+        title="Dex Download"
         // onPress={() => DexDownload('BlueSnap DB9 (7B8294)')}
         onPress={() => DexDownload('C8:48:D3:7B:82:94')}
       />
